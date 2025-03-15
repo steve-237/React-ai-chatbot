44ddeee966f69c01f378a4f1d0a3ad467c7612b5
steve-237 <steve0peg@gmail.com>
2025-03-15 20:00:57 +0100

    feat: connect google ai for chatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import {useState} from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
 import styles from "./App.module.css";
 
+const googleai = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_AI_API_KEY);
+const gemini = googleai.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = gemini.startChat({ history: [] });
+
 function App() {
   const [messages, setMessages] = useState([]);
 
-  function handleContentSend(content) {
-    setMessages((prevMessages) => [...prevMessages, { content,  role: 'user' }])
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content,  role: 'user' });
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({ content: result.response.text(),  role: 'assistant' });
+    } catch (error) {
+      addMessage({ content:"Sorry, I couldn't process your request. Please try again" + error,  role: 'system' });
+    }
   }
 
   return (
