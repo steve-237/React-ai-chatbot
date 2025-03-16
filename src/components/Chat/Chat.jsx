273a54ec55d9e27b75d13d966a3da39c98eb6d57
steve-237 <steve0peg@gmail.com>
2025-03-16 17:53:09 +0100

    fix: auto-scroll to bottom

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,12 +1,19 @@
+import { useRef, useEffect } from "react";
 import Markdown from "react-markdown";
 import styles from "./Chat.module.css";
 
 const WELCOME_MESSAGE = {
-    role: "assistant",
-    content: "Hello! How can L assist you right now?",
-}
+  role: "assistant",
+  content: "Hello! How can L assist you right now?",
+};
 
 export function Chat({ messages }) {
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ bahavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className={styles.Chat}>
       {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
@@ -14,6 +21,8 @@ export function Chat({ messages }) {
           <Markdown>{content}</Markdown>
         </div>
       ))}
+
+      <div ref={messagesEndRef} />
     </div>
   );
 }
