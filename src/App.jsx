d7aea9cd4a908606bb9d79905e8e22813094aef3
steve-237 <steve0peg@gmail.com>
2025-03-16 16:02:15 +0100

    feat: display loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
-import styles from "./App.module.css";
 import { Assistant } from "./components/assistants/googleai";
+import { Loader } from "./components/Loader/Loader";
+import styles from "./App.module.css";
 
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
   }
 
   async function handleContentSend(content) {
-    addMessage({ content,  role: 'user' });
+    addMessage({ content, role: "user" });
+    setIsLoading(true);
     try {
       const result = await assistant.chat(content);
-      addMessage({ content: result,  role: 'assistant' });
+      addMessage({ content: result, role: "assistant" });
     } catch (error) {
-      addMessage({ content:"Sorry, I couldn't process your request. Please try again" + error,  role: 'system' });
+      addMessage({
+        content:
+          "Sorry, I couldn't process your request. Please try again" + error,
+        role: "system",
+      });
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
-   <div className={styles.App}>
-    <header className={styles.Header}>
-      <img className={styles.Logo} src="/chat-bot.png" />
-      <h2 className={styles.Title}>AI Chatbot</h2>
-    </header>
-    <div className={styles.ChatContainer}>
-      <Chat messages={messages}/>
+    <div className={styles.App}>
+      {isLoading && <Loader />}
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls onSend={handleContentSend} />
     </div>
-    <Controls onSend={handleContentSend} />
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
