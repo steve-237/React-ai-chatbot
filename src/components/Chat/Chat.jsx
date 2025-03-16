c39808e719d6c562fedd72c13a0fb44f95a59715
steve-237 <steve0peg@gmail.com>
2025-03-16 20:32:10 +0100

    update: auto-scrolling behavior

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,26 +1,43 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useMemo } from "react";
 import Markdown from "react-markdown";
 import styles from "./Chat.module.css";
 
-const WELCOME_MESSAGE = {
-  role: "assistant",
-  content: "Hello! How can L assist you right now?",
-};
+const WELCOME_MESSAGE_GROUP = [
+  {
+    role: "assistant",
+    content: "Hello! How can I assist you right now?",
+  },
+];
 
 export function Chat({ messages }) {
   const messagesEndRef = useRef(null);
+  const messagesGroups = useMemo(
+    () =>
+      messages.reduce((groups, message) => {
+        if (message.role === "user") groups.push([]);
+        groups[groups.length - 1].push(message);
+        return groups;
+      }, []),
+    [messages]
+  );
 
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ bahavior: "smooth" });
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
   return (
     <div className={styles.Chat}>
-      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-        <div key={index} className={styles.Message} data-role={role}>
-          <Markdown>{content}</Markdown>
-        </div>
-      ))}
+      {[WELCOME_MESSAGE_GROUP, ...messagesGroups].map(
+        (messages, groupIndex) => (
+          <div key={groupIndex} className={styles.Group}>
+            {messages.map(({ role, content }, index) => (
+              <div key={index} className={styles.Message} data-role={role}>
+                <Markdown>{content}</Markdown>
+              </div>
+            ))}
+          </div>
+        )
+      )}
 
       <div ref={messagesEndRef} />
     </div>
